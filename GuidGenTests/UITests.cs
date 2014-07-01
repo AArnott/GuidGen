f9172d039dcc0f14ed6beb97178d70238d61a14b
Andrew Arnott <andarno@microsoft.com>
2014-07-01 12:15:29 -0700

    Add UI tests.

---
+++ GuidGenTests/UITests.cs
@@ -0,0 +1,65 @@
+﻿namespace GuidGenTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Controls;
+    using System.Windows.Threading;
+    using GuidGen;
+    using Xunit;
+
+    [Trait("UI", "")]
+    public class UITests
+    {
+        private const bool visibleUITests = false;
+
+        [Fact]
+        public void UIRespondsToViewModelChanges()
+        {
+            this.UITest(window =>
+            {
+                var listBox = (ListBox)window.FindName("FormatListBox");
+                var codeSnippetText = (TextBlock)window.FindName("CodeSnippetText");
+
+                foreach (CodeSnippetFormat format in Enum.GetValues(typeof(CodeSnippetFormat)))
+                {
+                    window.ViewModel.Format = format;
+                    var listBoxSelectedFormat = (CodeSnippetFormat)listBox.SelectedValue;
+                    Assert.Equal(format, listBoxSelectedFormat);
+                    Assert.Equal(window.ViewModel.CodeSnippet, codeSnippetText.Text);
+                }
+
+                return Task.FromResult<object>(null);
+            });
+        }
+
+        private void UITest(Func<MainWindow, Task> testMethod, bool visible = visibleUITests)
+        {
+            SynchronizationContext.SetSynchronizationContext(new DispatcherSynchronizationContext());
+            var window = new MainWindow();
+
+            var frame = new DispatcherFrame();
+            SynchronizationContext.Current.Post(async d =>
+            {
+                try
+                {
+                    if (visible)
+                    {
+                        window.Show();
+                    }
+
+                    await testMethod(window);
+                }
+                finally
+                {
+                    window.Close();
+                    frame.Continue = false;
+                }
+            }, null);
+            Dispatcher.PushFrame(frame);
+        }
+    }
+}
