f0c7444c99b67c8693e59c636f386666ec3e83d7
Andrew Arnott <andarno@microsoft.com>
2014-07-01 10:26:34 -0700

    Add test project with several tests.

---
+++ GuidGenTests/GuidGenViewModelTests.cs
@@ -0,0 +1,46 @@
+﻿namespace GuidGenTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GuidGen;
+    using Xunit;
+
+    public class GuidGenViewModelTests
+    {
+        private GuidGenViewModel viewModel = new GuidGenViewModel();
+
+        [Fact]
+        public void InitialGuidIsNonEmpty()
+        {
+            Assert.NotEqual(Guid.Empty, this.viewModel.Value);
+        }
+
+        [Fact]
+        public void NewGuidChangesGuid()
+        {
+            Guid originalGuid = this.viewModel.Value;
+            this.viewModel.NewGuid();
+            Assert.NotEqual(originalGuid, this.viewModel.Value);
+            Assert.NotEqual(Guid.Empty, this.viewModel.Value);
+        }
+
+        [Fact]
+        public void NewGuidCommandCanExecute()
+        {
+            Guid originalGuid = this.viewModel.Value;
+            Assert.True(this.viewModel.NewGuidCommand.CanExecute(null));
+        }
+
+        [Fact]
+        public void NewGuidCommandChangesGuid()
+        {
+            Guid originalGuid = this.viewModel.Value;
+            this.viewModel.NewGuidCommand.Execute(null);
+            Assert.NotEqual(originalGuid, this.viewModel.Value);
+            Assert.NotEqual(Guid.Empty, this.viewModel.Value);
+        }
+    }
+}
