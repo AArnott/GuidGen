5e95df39ae669b64b3122e73a86b7404caa3da5e
Andrew Arnott <andarno@microsoft.com>
2014-07-03 15:20:26 -0700

    Add VSPackage to activate the GuidGen tool.
    For some reason, the window is broken though. Even from the command line, I can't launch guidgen.exe

---
+++ GuidGenVsPackage/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+namespace Microsoft.GuidGenVsPackage
+{
+    using System;
+
+    static class GuidList
+    {
+        public const string guidGuidGenVsPackagePkgString = "f4938f46-b76f-4b6f-91a7-05978aab4426";
+        public const string guidGuidGenVsPackageCmdSetString = "c1712e1a-dc7d-4cf6-9982-13bfd42b80e9";
+
+        public static readonly Guid guidGuidGenVsPackageCmdSet = new Guid(guidGuidGenVsPackageCmdSetString);
+    };
+}
