3cf4fb1cf534e9767b9b2880c06218168ac042f2
Andrew Arnott <andarno@microsoft.com>
2014-07-03 17:12:35 -0700

    GuidGen window now re-surfaced on re-execution of the command.

--- GuidGenVsPackage/GuidGenVsPackagePackage.cs
+++ GuidGenVsPackage/GuidGenVsPackagePackage.cs
@@ -12,74 +12,69 @@
     using Microsoft.VisualStudio.Shell;
 
     /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    ///
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
-    /// register itself and its components with the shell.
+    /// Exposes the GuidGen window as a command in VS.
     /// </summary>
-    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-    // a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    // This attribute is used to register the information needed to show this package
-    // in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidGuidGenVsPackagePkgString)]
     public sealed class GuidGenVsPackagePackage : Package
     {
+        private GuidGen.MainWindow window;
+
         /// <summary>
-        /// Default constructor of the package.
-        /// Inside this method you can place any initialization code that does not require 
-        /// any Visual Studio service because at this point the package object is created but 
-        /// not sited yet inside Visual Studio environment. The place to do all the other 
-        /// initialization is the Initialize method.
+        /// Initializes a new instance of the <see cref="GuidGenVsPackagePackage"/> class.
         /// </summary>
         public GuidGenVsPackagePackage()
         {
             Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
         }
 
-
-
-        /////////////////////////////////////////////////////////////////////////////
-        // Overridden Package Implementation
-        #region Package Members
-
         /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// Called when the VSPackage is loaded by Visual Studio.
         /// </summary>
         protected override void Initialize()
         {
-            Debug.WriteLine (string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
             base.Initialize();
 
             // Add our command handlers for menu (commands must exist in the .vsct file)
             OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if ( null != mcs )
+            if (null != mcs)
             {
                 // Create the command for the menu item.
                 CommandID menuCommandID = new CommandID(GuidList.guidGuidGenVsPackageCmdSet, (int)PkgCmdIDList.cmdidCreateGuid);
-                MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID );
-                mcs.AddCommand( menuItem );
+                MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                mcs.AddCommand(menuItem);
             }
         }
-        #endregion
 
         /// <summary>
-        /// This function is the callback used to execute a command when the a menu item is clicked.
-        /// See the Initialize method to see how the menu item is associated to this function using
-        /// the OleMenuCommandService service and the MenuCommand class.
+        /// Releases the resources used by the <see cref="T:Microsoft.VisualStudio.Shell.Package" /> object.
+        /// </summary>
+        /// <param name="disposing">true if the object is being disposed, false if it is being finalized.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (this.window != null)
+            {
+                this.window.Close();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Opens the GuidGen window.
         /// </summary>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            var window = new GuidGen.MainWindow();
-            window.Show();
+            if (this.window == null)
+            {
+                this.window = new GuidGen.MainWindow();
+                this.window.Closed += (s, args) => this.window = null;
+            }
+
+            this.window.Show();
         }
     }
 }
